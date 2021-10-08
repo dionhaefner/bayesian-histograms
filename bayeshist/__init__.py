f38efd67c5f8c4f5add8a2f330695be42f8cef05
Dion Häfner <dion.haefner@nbi.ku.dk>
2021-10-08 12:18:24 +0200

    switch to valid placeholder version

--- bayeshist/__init__.py
+++ bayeshist/__init__.py
@@ -1,5 +1,5 @@
 from .bayeshist import bayesian_histogram
 from .plotting import plot_bayesian_histogram
 
-__version__ = "develop"
+__version__ = "v0.0+dev"
 __all__ = ["bayesian_histogram", "plot_bayesian_histogram"]
