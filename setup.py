d184325fea444954ab85c25c6d99973f60674b1c
Dion Häfner <dion.haefner@nbi.ku.dk>
2021-10-08 12:15:41 +0200

    fix version detection

--- setup.py
+++ setup.py
@@ -4,18 +4,32 @@ from distutils.core import setup
 
 from codecs import open
 import os
-
-from bayeshist import __version__
+import re
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+
+# read version from __init__.py
+version_file = os.path.join(here, "bayeshist", "__init__.py")
+version_pattern = re.compile(r"__version__\s*=\s*[\"'](.+)[\"']")
+
+with open(version_file, encoding="utf-8") as f:
+    for line in f:
+        match = version_pattern.match(line)
+        if match:
+            version = match.group(1)
+            break
+    else:
+        raise RuntimeError("Could not determine version from __init__.py")
+
+
 setup(
     name="bayeshist",
     license="MIT",
-    version=__version__,
+    version=version,
     description="Bayesian histograms for estimation of binary event rates",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -28,4 +42,5 @@ setup(
         "scipy",
     ],
     python_requires=">=3.6",
+    zip_safe=False,
 )
