403bf18e97cf349ae9cf4694c07f46e0f1031ec2
Dion Häfner <dion.haefner@nbi.ku.dk>
2021-10-08 12:01:10 +0200

    add metadata and auto deploy action

--- bayeshist/__init__.py
+++ bayeshist/__init__.py
@@ -1,4 +1,5 @@
 from .bayeshist import bayesian_histogram
 from .plotting import plot_bayesian_histogram
 
+__version__ = "develop"
 __all__ = ["bayesian_histogram", "plot_bayesian_histogram"]
--- setup.py
+++ setup.py
@@ -2,10 +2,23 @@
 
 from distutils.core import setup
 
+from codecs import open
+import os
+
+from bayeshist import __version__
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="bayeshist",
-    version="0.1",
+    license="MIT",
+    version=__version__,
     description="Bayesian histograms for estimation of binary event rates",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Dion Häfner",
     author_email="mail@dionhaefner.de",
     url="https://github.com/dionhaefner/bayeshist",
